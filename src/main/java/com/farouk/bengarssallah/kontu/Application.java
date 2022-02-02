ebc62a5e846a4e2b7378d33aeac7491f5069561d
farouk <farouk.bengarssallah@gmail.com>
2022-02-02 21:04:54 +0100

    final

--- src/main/java/com/farouk/bengarssallah/kontu/Application.java
+++ src/main/java/com/farouk/bengarssallah/kontu/Application.java
@@ -7,8 +7,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -18,14 +16,12 @@ import org.springframework.security.crypto.bcrypt.BCrypt;
 @EntityScan(value={"com.farouk.bengarssallah.kontu.domain"})
 @EnableJpaRepositories(value={"com.farouk.bengarssallah.kontu.repository"})
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 	
     @Autowired
     UserRepository userRepository;
 
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(new Class[]{Application.class});
-    }
+   
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, (String[])args);
